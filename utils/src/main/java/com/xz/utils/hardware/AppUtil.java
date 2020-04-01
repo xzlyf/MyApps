5ebf9243b17d32e23c810e067d7a505fefc11957
深圳--实习--陈泽荣 <1076409897@qq.com>
2020-04-01 11:37:36 +0800

    新增base64Utils

---
+++ utils/src/main/java/com/xz/utils/hardware/AppUtil.java
@@ -0,0 +1,39 @@
+package com.xz.utils.hardware;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Process;
+
+import java.util.List;
+
+/**
+ * @author czr
+ * @date 2020/3/31
+ */
+public class AppUtil {
+
+
+    /**
+     * 判断自己（此App）是否正在运行
+     *
+     * @param context
+     * @return
+     */
+    public static boolean shouldInit(Context context) {
+        ActivityManager am = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE));
+        List<ActivityManager.RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
+        String mainProcessName = context.getPackageName();
+
+        //获取本App的唯一标识
+        int myPid = Process.myPid();
+        //利用一个增强for循环取出手机里的所有进程
+        for (ActivityManager.RunningAppProcessInfo info : processInfos) {
+            //通过比较进程的唯一标识和包名判断进程里是否存在该App
+            if (info.pid == myPid && mainProcessName.equals(info.processName)) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
